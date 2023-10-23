9475df80cf5722ddee9f6507b14d4d1a67228f34
王海洋 <674468132@qq.com>
2023-10-23 15:38:33 +0800

    1

---
+++ webhook.php
@@ -0,0 +1,16 @@
+<?php
+	$secret = "";
+
+	$path = "www/wwwroot/xj.com";
+
+	$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+
+	if ($signature){
+		$hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"),$secret);
+		if (strcmp($signature, $hash) == 0){
+			echo shell_exec("cd {$path} && /usr/bin/git reset--hard origin/master && /usr/bin/git clean -f &&/usr/bin/git pu11 2>&1");
+			exit();
+		}
+	}
+
+http_response_code(404);
